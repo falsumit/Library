79ffcd3cf522c87842b1f5f87ca0b7d73c0a37ce
falsumit <bclarke516@outlook.com>
2023-10-17 13:37:58 -0400

    buttons functional, now able to add data to a new object which is added to the library array. need to create new book div with each new object entry

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
+const popup = document.getElementById('popup');
+
 // array for all the book objects
 const myLibrary = [];
 
 // constructor for a new book object
-function Book(title, author, genre, release) {
-    this.title = title;
-    this.author = author;
-    this.genre = genre;
-    this.release = release;
-    this.synopsis = function() {
+class Book {
+    constructor(title, author, genre, release) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.release = release;
+    }
+
+    synopsis() {
         return `The book ${title} was written by ${author} in ${release}.`
-    };
-};
-
-const book1 = new Book('Hello World', 'Jo Mama', 'action', 1990);
-
-function addBookToLibrary() {
-    const popup = document.getElementById('popup');
-    popup.style = 'visibility: visible';
-};
-
-function cancelSubmitBook() {
-    const popup = document.getElementById('popup');
-    popup.style = 'visibility: hidden';
+    }
 }
+
+
+
+const addBook = (e) => {
+    e.preventDefault();
+    let book = new Book(
+        document.getElementById('book_title').value,
+        document.getElementById('book_author').value,
+        document.getElementById('book_genre').value,
+        document.getElementById('book_release').value
+    );
+
+    myLibrary.push(book);
+    document.querySelector('form').reset();
+
+    console.log('added', {myLibrary});
+    let title = document.querySelector('.book .title');
+    title.textContent = myLibrary[0].title;
+
+    let author = document.querySelector('.book .author');
+    author.textContent = myLibrary[0].author;
+
+    let genre = document.querySelector('.book .genre');
+    genre.textContent = myLibrary[0].genre;
+
+    let release = document.querySelector('.book .release');
+    release.textContent = myLibrary[0].release;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('new_book').addEventListener('click', () => {
+        popup.style = 'visibility: visible';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('cancel_book').addEventListener('click', () => {
+        popup.style = 'visibility: hidden';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('add_book').addEventListener('click', addBook);
+});
