7fe448bbfd105de67042f0ccbefe40ac37ad3d6a
falsumit <bclarke516@outlook.com>
2023-10-21 16:22:17 -0400

    library fully functional, minus data saving. style complete as well

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// array for all the book objects
-
 const library = [];
 
-// constructor for a new book object
 class Book {
-    constructor(title, author, genre, release) {
+    constructor(title, author, status) {
         this.title = title;
         this.author = author;
-        this.genre = genre;
-        this.release = release;
+        this.status = status;
     }
 }
 
 const form = document.querySelector('form');
+const navBtn = document.getElementById('nav-btn');
+const cancel = document.getElementById('cancel');
 
-// function to add a new book
+navBtn.addEventListener('click', () => {
+    form.style = 'display: block';
+});
 
-const addBook = form.addEventListener('submit', (event) => {
+cancel.addEventListener('click', () => {
+    form.style = 'display: none';
+});
+
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
-    // let book = new Book(
-    //     document.getElementById('book_title').value,
-    //     document.getElementById('book_author').value,
-    //     document.getElementById('book_genre').value,
-    //     document.getElementById('book_release').value
-    // );
 
     const bookData = new FormData(form);
     let book = new Book();
@@ -32,99 +29,53 @@ const addBook = form.addEventListener('submit', (event) => {
     library.push(book);
 
     document.querySelector('form').reset();
+    form.style = 'display: none';
 
     let content = document.querySelector('#content');
-    let firstBook = document.querySelector('#first-book');
 
-    if (firstBook !== null) {
-        let title = document.querySelector('.book .title');
-        title.textContent = library[library.length - 1].title;
-    
-        let author = document.querySelector('.book .author');
-        author.textContent = library[library.length - 1].author;
-    
-        let genre = document.querySelector('.book .genre');
-        genre.textContent = library[library.length - 1].genre;
-    
-        let release = document.querySelector('.book .release');
-        release.textContent = library[library.length - 1].release;
+    let newBlock = document.createElement('div');
+    let h3 = document.createElement('h3');
+    let p = document.createElement('p');
+    let sBtn = document.createElement('button');
+    let rmBtn = document.createElement('button');
 
-        firstBook.removeAttribute('id', 'first-book');
+    content.appendChild(newBlock);
+    newBlock.appendChild(h3);
+    newBlock.appendChild(p);
+    newBlock.appendChild(sBtn);
+    newBlock.appendChild(rmBtn);
+
+    newBlock.classList.toggle('block');
+    h3.textContent = library[library.length - 1].title;
+    p.textContent = library[library.length - 1].author;
+    sBtn.setAttribute('type', 'button');
+    if (library[library.length - 1].status === 'unread') {
+        sBtn.classList.toggle('unread');
+        sBtn.textContent = 'Unread';
     } else {
-        let newBookDiv = document.createElement('div');
-        newBookDiv.classList.toggle('book');
-        content.appendChild(newBookDiv);
-    
-        let newBookTitle = document.createElement('h3');
-        newBookTitle.classList.toggle('title');
-        newBookDiv.appendChild(newBookTitle);
-        newBookTitle.textContent = library[library.length - 1].title;
-    
-        let newBookAuthor = document.createElement('p');
-        newBookAuthor.classList.toggle('author');
-        newBookDiv.appendChild(newBookAuthor);
-        newBookAuthor.textContent = library[library.length - 1].author;
-    
-        let newBookGenre = document.createElement('p');
-        newBookGenre.classList.toggle('genre');
-        newBookDiv.appendChild(newBookGenre);
-        newBookGenre.textContent = library[library.length - 1].genre;
-    
-        let newBookRelease = document.createElement('p');
-        newBookRelease.classList.toggle('release');
-        newBookDiv.appendChild(newBookRelease);
-        newBookRelease.textContent = library[library.length - 1].release;
-
-        let newBookButtons = document.createElement('div');
-        newBookButtons.classList.toggle('book-buttons');
-        newBookDiv.appendChild(newBookButtons);
-
-        let newReadButton = document.createElement('button');
-        newReadButton.classList.toggle('read-toggle');
-        newReadButton.classList.toggle('unread');
-        newBookButtons.appendChild(newReadButton);
-        newReadButton.textContent = 'Unread';
-
-        let newRemoveButton = document.createElement('button');
-        newRemoveButton.classList.toggle('remove-book');
-        newBookButtons.appendChild(newRemoveButton);
-        newRemoveButton.textContent = 'Remove';
+        sBtn.classList.toggle('read');
+        sBtn.textContent = 'Read';
     }
-});
+    rmBtn.setAttribute('type', 'button');
+    rmBtn.classList.toggle('rm-book');
+    rmBtn.textContent = 'Remove Book';
 
-// button controls
-
-const popup = document.getElementById('popup');
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('new_book').addEventListener('click', () => {
-        popup.style = 'display: block';
+    sBtn.addEventListener('click', () => {
+        if (sBtn.textContent === 'Unread') {
+            sBtn.textContent = 'Read';
+            sBtn.classList.toggle('read');
+            sBtn.classList.toggle('unread');
+        } else {
+            sBtn.textContent = 'Unread';
+            sBtn.classList.toggle('unread');
+            sBtn.classList.toggle('read');
+        }
     });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('cancel_book').addEventListener('click', () => {
-        popup.style = 'display: none';
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('add_book').addEventListener('click', addBook);
-    document.getElementById('add_book').addEventListener('click', () => {
-        popup.style = 'display: none';
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.read-toggle').forEach(readButton => {
-        readButton.addEventListener('click', () => {
-            readButton.classList.toggle('unread');
-            readButton.classList.toggle('read');
-        
-            if(readButton.classList.contains('read')) {
-                readButton.textContent = 'Read';
-            } else {
-                readButton.textContent = 'Unread';
-            }
-        });
+    rmBtn.addEventListener('click', () => {
+        let rmConf = 'Are you sure you want to remove this book?';
+        if (confirm(rmConf) == true) {
+            newBlock.remove();
+        }
     });
 });
